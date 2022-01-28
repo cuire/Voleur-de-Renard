374a520bc41642cc014e08caac3977cc36e7e02d
Matvey Bochkov <bochkovmatvei@bk.ru>
2022-01-29 00:40:18 +0500

    Minor fixes to gui. Edited scene behaviour.

--- scenes/main_menu/menu.gd
+++ scenes/main_menu/menu.gd
@@ -4,11 +4,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_ButtonNewGame_pressed():
-	var gui_difficulty_selection = load("res://scenes/main_menu/difficulty_selection.tscn").instance()
-	get_tree().current_scene.add_child(gui_difficulty_selection)
+	#var gui_difficulty_selection = load("res://scenes/main_menu/difficulty_selection.tscn").instance()
+	#get_tree().current_scene.add_child(gui_difficulty_selection)
 	# show difficulty selection gui
-	# get_tree().change_scene("res://scenes/main_location/kingdom.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene("res://scenes/main_menu/difficulty_selection.tscn")
+	
 
 func _on_ButtonExit_pressed():
 	get_tree().quit()
