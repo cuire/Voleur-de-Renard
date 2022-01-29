4512c41380c7c13482fd8a3b79cf29b663d5a2e4
cuire <garwes@icloud.com>
2022-01-29 20:32:29 +0300

    frame creater

--- scenes/main_location/kingdom.gd
+++ scenes/main_location/kingdom.gd
@@ -6,32 +6,37 @@ onready var prince = $Prince
 onready var counter = $Control/MoneyCounter
 onready var viewport_size: float = get_viewport().size.x
 var current_position: float = 0
+var new_frame
 
+var frame_1 = preload("res://scenes/main_location/frames/frame_1.tscn")
 var bakery = preload("res://minigames/bakery/bakery.tscn")
 
 const DIRECTION: int = -1
 export var velocity: float = 1.5 * DIRECTION
 
 func _process(delta: float) -> void:
+	new_frame = frame_1.instance()
 	current_position += velocity * delta * DIRECTION
-	_move_frame(current_frame, delta)
-	_move_frame(next_frame, delta)
+	_move_frame(new_frame, delta)
 	if Input.is_action_pressed("ui_right"):
 		add_obstacle(next_frame, bakery.instance())
 
 # Function to move frame
-func _move_frame(frame: Node2D, delta) -> void:
-	# Move frame
-	frame.position.x += velocity * delta
+func _move_frame(next_frame_to_render: Node2D, delta) -> void:
+	# Move frames
+	current_frame.position.x += velocity * delta
+	next_frame.position.x += velocity * delta
 	# Try to reposition the frame
-	if viewport_size * DIRECTION > frame.position.x:
-		frame.position.x += 2 * viewport_size
+	if viewport_size * DIRECTION > current_frame.position.x:
+		remove_child(current_frame)
+		add_child(next_frame_to_render)
+		move_child(next_frame_to_render, 0)
+		current_frame = next_frame
+		next_frame = next_frame_to_render
+		next_frame.position.x = viewport_size
 
 func add_obstacle(frame: Node2D, obstacle: Minigame):
 	obstacle.position = Vector2(randi()%900+100, randi()%600+250)
-	# check if any collisions
-
-	print("Added")
 	var _status = obstacle.connect("steal_money", prince, "_on_Steal_Money")
 	frame.add_child(obstacle)
 
